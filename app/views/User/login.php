068ba42f4aaab8987389a2b505fb734b336568df
sergeisavelyev <sergeisavelyev11@gmail.com>
2023-02-06 13:59:40 +0300

    added required attr

--- app/views/User/login.php
+++ app/views/User/login.php
@@ -4,11 +4,11 @@
         <p><?php __('user_login') ?> <?php __('user_login_or') ?> <a href="/user/signup" class="text-decoration-underline"><?php __('user_login_signup') ?></a></p>
         <div class="mb-2">
             <label class="form-label"><?php __('user_login_input_email') ?></label>
-            <input type="text" class="form-control" name="email">
+            <input type="text" class="form-control" name="email" required>
         </div>
         <div class="mb-2">
             <label class="form-label"><?php __('user_login_input_password') ?></label>
-            <input type="password" class="form-control" name="password">
+            <input type="password" class="form-control" name="password" required>
         </div>
         <button type="submit" class="btn btn-outline-warning w-100 mt-3"><?php __('user_login') ?></button>
     </form>
--- app/views/User/signup.php
+++ app/views/User/signup.php
@@ -4,15 +4,15 @@
         <p><?php __('user_signup_btn') ?> <?php __('user_signup_or') ?> <a href="/user/login" class="text-decoration-underline"><?php __('user_signup_login_btn') ?></a></p>
         <div class="mb-2">
             <label class="form-label"><?php __('user_signup_input_email') ?></label>
-            <input type="text" class="form-control" name="email" id="email">
+            <input type="text" class="form-control" name="email" id="email" required>
         </div>
         <div class="mb-2">
             <label class="form-label"><?php __('user_signup_input_login') ?></label>
-            <input type="text" class="form-control" name="login" id="login">
+            <input type="text" class="form-control" name="login" id="login" required>
         </div>
         <div class="mb-2">
             <label class="form-label"><?php __('user_signup_input_password') ?></label>
-            <input type="password" class="form-control" name="password" id="password">
+            <input type="password" class="form-control" name="password" id="password" required>
         </div>
         <button type="submit" class="btn btn-outline-warning w-100 mt-3"><?php __('user_signup_btn') ?></button>
     </form>
