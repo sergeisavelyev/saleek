5150187b637e3af273b036dddb776a389b03f93f
sergeisavelyev <sergeisavelyev11@gmail.com>
2023-02-16 10:18:14 +0300

    add saveOrderProduct

--- app/models/Order.php
+++ app/models/Order.php
@@ -15,15 +15,29 @@ class Order extends AppModel
                 'note' => $data['note'],
                 'total' => $_SESSION['cart.sum'],
             ];
-
             Db::query("INSERT INTO orders (user_id, note, total) VALUE (:user_id, :note, :total)", $params);
             $order_id = Db::lastId();
+            self::saveOrderProduct($order_id);
 
             Db::commit();
             return $order_id;
         } catch (\Exception $e) {
             Db::rollback();
             return false;
+        }
+    }
+
+    public static function saveOrderProduct($order_id)
+    {
+        foreach ($_SESSION['cart'] as $product_id => $product) {
+            $params = [
+                'order_id' => $order_id,
+                'product_id' => $product_id,
+                'title' => $product['title'],
+                'price' => $product['price'],
+                'sum' => $product['price'],
+            ];
+            Db::query("INSERT INTO order_product (order_id, product_id, title, price, sum) VALUE (:order_id, :product_id, :title, :price, :sum)", $params);
         }
     }
 }
